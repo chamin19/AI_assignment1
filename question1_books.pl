2e2681dbc3e8a89f2ce2fb98cc295b218c83548b
camillia <hanaan.amin@yahoo.com>
2022-09-21 23:46:10 -0400

    added atomic statements for q1 part 1

--- question1_books.pl
+++ question1_books.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Camillia Amin 
+%%%%% NAME: Rudaba Syed
+%%%%% NAME: Mayra Mussarrat
 %
 % Add the required atomic propositions and rules in the corresponding sections. 
 % If you put the propositions and rules in the wrong sections, you may lose marks.
@@ -13,15 +13,45 @@
 
 %%%%% ATOMIC: authorOf
 % Add the atomic propositions for authorOf in this section
-
+authorOf(chimamandaNgoziAdichie, halfofaYellowSun).
+authorOf(emilySt.JohnMandel, stationEleven).
+authorOf(mordecaiRichler, theApprenticeshipofDuddyKravitz).
+authorOf(mordecaiRichler, barneysVersion).
+authorOf(pauloCoelho,theAlchemist).
+authorOf(pauloCoelho,brida).
+authorOf(harukiMurakami,aWildSheepChase).
+authorOf(harukiMurakami,hearTheWindSing).
+authorOf(harukiMurakami,pinball).
+authorOf(jkrowling,theCuckoosCalling).
+authorOf(jkrowling,theSilkworm).
+authorOf(jkrowling,careerOfEvil).
+authorOf(stephenKing,carrie).
 
 %%%%%  ATOMIC: publishedBy
 % Add the atomic propositions for publishedBy in this section
-
+publishedBy(penguin,theAlchemist).
+publishedBy(springerNature,brida).
+publishedBy(randomHouse,theAlchemist).
+publishedBy(harperCollins,brida).
+publishedBy(warnerbors,theCuckoosCalling).
+publishedBy(warnerbros,theSilkworm).
+publishedBy(warnerbros,careerOfEvil).
+publishedBy(warnerbros,carrie).
+publishedBy(macmillanPublishers,carrie).
+publishedBy(harperCollins,carrie).
 
 %%%%% ATOMIC: publicationYear
 % Add the atomic propositions for publicationYear in this section
-
+publicationYear(theAlchemist,2019).
+publicationYear(brida,2019).
+publicationYear(aWildSheepChase,1982).
+publicationYear(hearTheWindSing,1979).
+publicationYear(pinball,1980).
+publicationYear(stationEleven, 1995).
+publicationYear(theCuckoosCalling,2013).
+publicationYear(theSilkworm,2014).
+publicationYear(careerOfEvil,2015).
+publicationYear(carrie,1974).
 
 %%%%% END
 % DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
