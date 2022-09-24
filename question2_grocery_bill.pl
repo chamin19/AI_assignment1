3276e565995ee73d41890ccf71b1cbe21b7120c9
rudabada <71153954+rudabada@users.noreply.github.com>
2022-09-23 23:01:17 -0400

    Update question2_grocery_bill.pl

--- question2_grocery_bill.pl
+++ question2_grocery_bill.pl
@@ -60,7 +60,7 @@ costAfterTaxAndSale(Item, AfterSaleAndTax) :- cost(Item,P), \+ taxable(Item), tw
 costAfterTaxAndSale(Item, AfterSaleAndTax) :- cost(Item,P), taxable(Item), taxRate(Rate), \+ twoForOneSale(Item), numPurchased(Item,N), N1 is N, N1>1, AfterSaleAndTax is ((P*N)*(1+Rate)).
 %(taxable, BOGO, E)
 costAfterTaxAndSale(Item, AfterSaleAndTax) :- cost(Item,P), \+ taxable(Item), taxRate(Rate), twoForOneSale(Item), numPurchased(Item,N), N1 is N, N1>1, E is N1 mod 2, E =:= 0, AfterSaleAndTax is ((P*(N/2))*(1+Rate)).
-%(taxable, BOGO, O)
+%(taxable, BOGO, O) ***NEEDS DEBUGGING
 costAfterTaxAndSale(Item, AfterSaleAndTax) :- cost(Item,P), \+ taxable(Item), taxRate(Rate), twoForOneSale(Item), numPurchased(Item,N), N1 is N, N1>1, O is N1 mod 2, O =:= 1, AfterSaleAndTax is ((P*((N+1)/2))*(1+Rate)).
 
 
